323b7d2bc7a40a05b7c431d42f70eb11fc99cd94
Deep Siroya <siroya.d@northeastern.edu>
2023-10-25 18:28:51 -0400

    Change pkr shell

--- pkr/pkg-install.sh
+++ pkr/pkg-install.sh
@@ -20,8 +20,16 @@ cd /opt/webapp
 sudo npm i
 
 sudo mv /tmp/node-app.service /lib/systemd/system/node-app.service 
+
+sudo groupadd csye6225
+sudo useradd -s /bin/false -g csye6225 -d /opt/webapp -m csye6225
+sudo chown -R csye6225:csye6225 /opt/webapp
+sudo chmod -R 755 /opt/webapp
+
 sudo systemctl daemon-reload
 sudo systemctl enable node-app.service
 sudo systemctl start node-app.service
 
 sudo apt-get clean
+
+
