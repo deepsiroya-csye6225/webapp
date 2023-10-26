3014b57dbb542c4f620772515ed89b84a20a29da
Deep Siroya <53616046+deepsiroya17@users.noreply.github.com>
2023-10-26 15:38:22 -0400

    Feature 16 (#131)
    * Change pkr and service
    
    * Change pkr and service
    
    * Change pkr and service

--- pkr/pkg-install.sh
+++ pkr/pkg-install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 sudo apt update -y
 sudo apt upgrade -y
 
@@ -17,7 +19,7 @@ sudo npm i
 sudo mv /tmp/node-app.service /lib/systemd/system/node-app.service 
 
 sudo groupadd csye6225
-sudo useradd -s /bin/false -g csye6225 -d /opt/webapp csye6225
+sudo useradd -s /bin/false -g csye6225 -d /opt/webapp -m csye6225
 sudo chown -R csye6225:csye6225 /opt/webapp
 sudo chown -R csye6225:csye6225 /etc/environment
 sudo chmod -R 755 /opt/webapp
@@ -25,4 +27,4 @@ sudo chmod -R 755 /opt/webapp
 sudo systemctl daemon-reload
 sudo systemctl enable node-app.service
 sudo systemctl start node-app.service
-
+sudo systemctl restart node-app.service
