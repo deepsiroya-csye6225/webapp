0bd6ac5863f087d6276e54aef59d8aaaa74fc1a8
Deep Siroya <siroya.d@northeastern.edu>
2023-10-31 22:07:59 -0400

    Change shell script

--- pkr/pkg-install.sh
+++ pkr/pkg-install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo apt update
+sudo apt update -y
 sudo apt upgrade -y
 
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
@@ -14,17 +14,16 @@ sudo mkdir /opt/webapp
 sudo unzip /tmp/webapp.zip -d /opt/webapp
 cd /opt/webapp 
 
-
-
-sudo mv /tmp/node-app.service /lib/systemd/system/node-app.service 
+sudo npm i
 
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/webapp -m csye6225
+
 sudo chown -R csye6225:csye6225 /opt/webapp
 sudo chown -R csye6225:csye6225 /etc/environment
 sudo chmod -R 755 /opt/webapp
 
-sudo -u csye6225 npm i
+sudo mv /tmp/node-app.service /lib/systemd/system/node-app.service 
 
 sudo systemctl daemon-reload
 sudo systemctl start node-app
