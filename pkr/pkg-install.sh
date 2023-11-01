9ab3eb0130fa39bb2f54b6f99c0a8029be4b34b2
Deep Siroya <siroya.d@northeastern.edu>
2023-10-31 22:22:28 -0400

    Change shell script and workflow

--- pkr/pkg-install.sh
+++ pkr/pkg-install.sh
@@ -16,15 +16,14 @@ cd /opt/webapp
 
 sudo npm i
 
+sudo mv /tmp/node-app.service /lib/systemd/system/node-app.service 
+
 sudo groupadd csye6225
 sudo useradd -s /bin/false -g csye6225 -d /opt/webapp -m csye6225
-
 sudo chown -R csye6225:csye6225 /opt/webapp
 sudo chown -R csye6225:csye6225 /etc/environment
 sudo chmod -R 755 /opt/webapp
 
-sudo mv /tmp/node-app.service /lib/systemd/system/node-app.service 
-
 sudo systemctl daemon-reload
-sudo systemctl start node-app
-sudo systemctl enable node-app
+sudo systemctl start node-app.service
+sudo systemctl enable node-app.service
