6d9be2988c6abe23b14a5683ae40334181c2a543
Deep Siroya <siroya.d@northeastern.edu>
2023-11-06 00:02:39 -0500

    Change packer shell script

--- pkr/pkg-install.sh
+++ pkr/pkg-install.sh
@@ -6,7 +6,7 @@ sudo apt update -y
 sudo apt upgrade -y
 
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-sudo apt-get install -y nodejs 
+sudo apt-get install -y nodejs
 
 sudo apt install -y unzip
 
@@ -28,3 +28,13 @@ sudo mv /tmp/node-app.service /lib/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl start node-app
 sudo systemctl enable node-app
+
+sudo wget https://s3.amazonaws.com/amazoncloudwatch-agent/debian/amd64/latest/amazon-cloudwatch-agent.deb
+sudo dpkg -i amazon-cloudwatch-agent.deb || sudo apt-get install -y -f
+
+sudo mv /tmp/amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+sudo chown csye6225:csye6225 /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+sudo chmod 755 /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json
+
+sudo systemctl enable amazon-cloudwatch-agent.service
+sudo systemctl start amazon-cloudwatch-agent.service
